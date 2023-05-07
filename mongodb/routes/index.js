f0c4ed39d886ce0bde0c00c8c0d3ba5004922cc7
akmatov <128972220+1kmat0v@users.noreply.github.com>
2023-05-07 22:05:55 +0300

    Add files via upload

---
+++ mongodb/routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+const MongoClient = require("mongodb").MongoClient;
+   
+const url = "mongodb://127.0.0.1:27017/";
+const mongoClient = new MongoClient(url);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/toMongo", function(req,res){
+ 
+async function run() {
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("akm");
+        const collection = db.collection("lmk");
+        const info = {name: req.query.name, gmail: req.query.gmail, age: req.query.password};
+        const result = await collection.insertOne(info);
+        console.log(result);
+        console.log(info);
+    }catch(err) {
+        console.log(err);
+    } finally {
+        await mongoClient.close();
+    }
+}
+run().catch(console.error);
+
+console.log(req.query.name, req.query.gmail);
+console.log(req.query.password);
+res.render("111.hbs", {})
+})
+
+
+module.exports = router;
